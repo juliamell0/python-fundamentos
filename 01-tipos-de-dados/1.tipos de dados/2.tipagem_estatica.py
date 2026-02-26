d402ee00c820ecbaeaa66f8d4a7f96b2da0df66e
juliamell0 <julia.melissaguerreiro@gmail.com>
2026-02-26 14:26:55 -0300

    Add static typing example in tipagem_estatica.py

---
+++ 01-tipos-de-dados/1.tipos de dados/2.tipagem_estatica.py
@@ -0,0 +1,15 @@
+import os
+os.system("clear")
+# Definição de tipos obrigatória.
+
+nome: str # Equivalente ao caracter
+idade:int # Equivalente ao tipo inteiro
+altura: float # Equivalente ao tipo real
+
+nome = "Marta"
+idade = 23
+altura = 1.63
+
+print(nome)
+print(idade)
+print(altura)
