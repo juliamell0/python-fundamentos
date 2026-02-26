fe1c0e5107a44c2d883c24dd6af50094743cf1ff
juliamell0 <julia.melissaguerreiro@gmail.com>
2026-02-26 14:36:33 -0300

    Update 1.estrutura_simples.py

--- 2-estruturas-condicionais/1.estrutura_simples.py
+++ 2-estruturas-condicionais/1.estrutura_simples.py
@@ -1,1 +1,20 @@
+import os
+os.system("clear")
 
+idade = 15
+# se idade < 18
+#   escreval("Acesso negado.")
+#senao
+#   escreval("Acesso permitido.")
+#fimse
+
+if idade < 18:
+    print("Acesso negado.")
+    
+else:
+    print("Acesso negado.")
+
+
+    
+ 
+print("== Fim ==")
