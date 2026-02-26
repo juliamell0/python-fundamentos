78d12dddbdfceae52ccada3202970bd1e28974e0
juliamell0 <julia.melissaguerreiro@gmail.com>
2026-02-26 14:53:40 -0300

    Add files via upload

---
+++ 3-estrutura-de-repeticao/10.while.py
@@ -0,0 +1,16 @@
+import os
+os.system ("cls || clear")
+
+contador = 0
+continua = 's'
+
+contador += 1
+continua = input("Tecle 's' se deseja continuar.").strip().lower()
+if contador == 0:
+    print("O bloco NÃO foi repetido.")
+else:
+    print(f"O bloco foi repetido {contador} vezes .")
+
+while continua == 's':
+    #comandos a serem repetidos
+    print("Repetindo...")
