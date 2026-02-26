5195f27959a11583dfe106c60537d43b2e2e11bb
juliamell0 <julia.melissaguerreiro@gmail.com>
2026-02-26 14:33:07 -0300

    Add files via upload

---
+++ 1.tipos de dados/4.solicitando_dados.py
@@ -0,0 +1,11 @@
+import os
+os.system("clear")
+
+# Solicitando dados.
+nome =input("Digite sue nome: ")
+idade =int(input("Digite sua idade: "))
+altura =float(input("Digite sua altura: "))
+
+print(f"Nome: {nome} ")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
