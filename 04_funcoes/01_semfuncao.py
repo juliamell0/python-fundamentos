af9e9041df2b2226957f56fad3626d53321a2262
juliamell0 <julia.melissaguerreiro@gmail.com>
2026-02-26 22:45:27 -0300

    Add user input for name and age with clearing screen

---
+++ 04_funcoes/01_semfuncao.py
@@ -0,0 +1,17 @@
+import os
+os.system("cls ||clear")
+
+
+os.system("cls || clear")
+
+print("== SENAI ==")
+nome = input("Digite seu nome: ")
+
+os.system("cls || clear")
+print("== SENAI ==")
+idade = input("Digite seu idade: ")
+
+os.system("cls || clear")
+print("== SENAI ==")
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
