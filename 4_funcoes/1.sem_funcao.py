06c1ff6c2a705b0bb7007b423ac2ca67a9e5d9cc
juliamell0 <julia.melissaguerreiro@gmail.com>
2026-02-27 01:29:36 -0300

    Add user input for name and age with clear screen

--- 4_funcoes/1.sem_funcao.py
+++ 4_funcoes/1.sem_funcao.py
@@ -1,1 +1,16 @@
+import os
+os.system("cls || clear")
 
+os.system("cls || clear")
+
+print("== Bem Vindo ==")
+nome= input("Digite seu nome: ")
+
+os.system("cls || clear")
+print("== Bem Vindo ==")
+idade = int(input("Digite sua idade: "))
+
+os.system("cls || clear")
+print("== Bem Vindo ==")
+print(f"Nome: {nome}")
+print(f"idade: {idade}")
